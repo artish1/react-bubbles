b4883707ec68c1a8c6bb6dc09867abea7bfe316f
Mark Artishuk <markyshuk@gmail.com>
2019-11-15 08:26:26 -0800

    Added login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Paper,
+  Typography,
+  Button,
+  TextField,
+  makeStyles
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    width: "50%",
+    minWidth: 300,
+    maxWidth: 650
+  },
+  textField: {
+    marginBottom: theme.spacing(3)
+  },
+  formContainer: {
+    marginTop: 25,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%"
+  }
+}));
+
+const initialFormData = {
+  username: "",
+  password: ""
+};
 
 const Login = () => {
+  const [credentials, setCredentials] = useState(initialFormData);
+  const classes = useStyles();
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className={classes.formContainer}>
+      <Paper className={classes.root}>
+        <Typography variant="h2" component="h3">
+          Login
+        </Typography>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            name="username"
+            label="Username"
+            placeholder="Username"
+            id="username"
+            onChange={handleChange}
+            value={credentials.username}
+          />
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            label="Password"
+            placeholder="Password"
+            type="password"
+            name="password"
+            id="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </div>
   );
 };
 
